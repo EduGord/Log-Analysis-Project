0c441a5fc86287659630e748a0fbc54579eae3ab
EduGord <edu_gordilho@hotmail.com>
2018-08-21 13:11:33 -0300

    minor fix
    third question was not right

--- README.md
+++ README.md
@@ -641,7 +641,7 @@ query_3
 ```python
 questions = ["What are the most popular three articles of all time?",
              "Who are the most popular article authors of all time?",
-             "Who are the most popular article authors of all time?"]
+             "On which days did more than 1% of requests lead to errors?"]
 
 queries = [query_1, query_2, query_3]
 ```
--- log-analysis-summary.py
+++ log-analysis-summary.py
@@ -53,7 +53,7 @@ query_3 = cur.fetchall()
 
 questions = ["What are the most popular three articles of all time?",
              "Who are the most popular article authors of all time?",
-             "Who are the most popular article authors of all time?"]
+             "On which days did more than 1% of requests lead to errors?"]
 
 queries = [query_1, query_2, query_3]
 
